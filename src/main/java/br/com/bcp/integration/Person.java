0193c6c3f478b21cfe1069cebc1cf0f1e3b5f182
Bruno Proenca <bruno.proenca@synchro.com.br>
2020-05-15 19:03:31 -0300

    first commit

---
+++ src/main/java/br/com/bcp/integration/Person.java
@@ -0,0 +1,48 @@
+package br.com.bcp.integration;
+public class Person {
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+
+	private int id;
+	private String firstName;
+	private String lastName;
+
+	public Person() {
+		super();
+	}
+
+	public Person(int id, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
---
+++ src/main/java/br/com/bcp/integration/SpringIntegrationBcpApplication.java
@@ -0,0 +1,24 @@
+package br.com.bcp.integration;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@Configuration
+@ImportResource("integration-context.xml")
+public class SpringIntegrationBcpApplication implements ApplicationRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationBcpApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		
+	}
+
+}
