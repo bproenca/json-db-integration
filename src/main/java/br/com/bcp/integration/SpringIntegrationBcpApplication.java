a10139acd4ab06fa616834f2b143d545c3105a4f
Bruno Cesar Proença <bruno.proenca@synchro.com.br>
2020-05-27 09:17:18 -0300

    organize import

--- src/main/java/br/com/bcp/integration/SpringIntegrationBcpApplication.java
+++ src/main/java/br/com/bcp/integration/SpringIntegrationBcpApplication.java
@@ -1,7 +1,5 @@
 package br.com.bcp.integration;
 
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
